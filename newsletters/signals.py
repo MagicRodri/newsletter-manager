c5ca08219ce726ae1a0720ed5678618e80a3ae1e
Rodrigue CODO <codorol22@gmail.com>
2022-12-11 01:35:43 +0500

    Send retrial on failure

--- newsletters/signals.py
+++ newsletters/signals.py
@@ -9,14 +9,14 @@ from .tasks import distribute_newsletter
 
 def post_newsletter_save(instance,created,*args, **kwargs):
 
-    if created:
-        start = instance.start_at
-        end = instance.end_at
-        if timezone.now() < start:
-            distribute_newsletter.apply_async((instance.pk,), eta = start, expires=end)
 
-        elif start <= timezone.now() < end : 
-            distribute_newsletter.apply_async((instance.pk,), expires=end)
+    start = instance.start_at
+    end = instance.end_at
+    if timezone.now() < start:
+        distribute_newsletter.apply_async((instance.pk,), eta = start, expires=end)
+
+    elif start <= timezone.now() < end : 
+        distribute_newsletter.apply_async((instance.pk,), expires=end)
 
 
 post_save.connect(post_newsletter_save, sender=Newsletter)
--- newsletters/tasks.py
+++ newsletters/tasks.py
@@ -7,12 +7,13 @@ from .models import Newsletter,Message,Client
 
 TOKEN = os.environ.get('TOKEN')
 
-@shared_task
-def add(a,b):
-    return a+b
 
-@shared_task
-def send_message(newsletter_pk,client_pk):
+@shared_task(bind=True, autoretry_for=(requests.RequestException,Exception),retry_backoff=60*2,retry_backoff_max=60*60,max_retries=5)
+def send_message(self,newsletter_pk,client_pk):
+    """
+        Create and send message to client with newsletter body attribute
+        Retry sending on any exception, 5 times with variable interval of time before given up
+    """
 
     endpoint = 'https://probe.fbrq.cloud/v1/send/{id}'
     header = {
@@ -20,7 +21,9 @@ def send_message(newsletter_pk,client_pk):
     }
     newsletter = Newsletter.objects.get(pk = newsletter_pk)
     client = Client.objects.get(pk=client_pk)
-    message = Message.objects.create(client=client, newsletter=newsletter)
+    message,_ = Message.objects.get_or_create(client=client, newsletter=newsletter)
+
+    # message to the endpoint
     msg = {
         'id' : message.id ,
         'phone':int(client.phone_number),
@@ -34,14 +37,16 @@ def send_message(newsletter_pk,client_pk):
         message.status = message.SENT
         message.save()
         return True
+    else:
+        raise Exception()
 
-    return False
 
 @shared_task
 def distribute_newsletter(newsletter_pk):
     """
+        Grab matched clients to the given newsletter and broadcast the message to them
     """
     newsletter = Newsletter.objects.get(pk = newsletter_pk)
     clients = newsletter.get_clients()
     for client in clients:
-        send_message.delay(newsletter_pk,client.pk)
+        succeeded = send_message.apply_async((newsletter_pk,client.pk))
